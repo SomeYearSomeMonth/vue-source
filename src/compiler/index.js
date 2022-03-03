b022f1e9fefd5866815d824af72d3088b3091523
majintao <277425255@qq.com>
2022-03-03 17:26:06 +0800

    feat:html-parser

--- src/compiler/index.js
+++ src/compiler/index.js
@@ -10,7 +10,84 @@ const attribute =
 const startTagClose = /^\s*(\/?)>/
 const defaultTagRE = /\{\{((?:.|\r?\n)+?)\}\}/g
 
+function start(tagName, attrs) {
+    console.log('开始标签', tagName, 'attrs', attrs)
+}
+
+function chars(text) {
+    console.log('文本是', text)
+}
+
+function end(tagName) {
+    console.log('结束标签', tagName)
+}
+
+function parseHTML(html) {
+    // 不停的去解析html字符串
+    while (html) {
+        let textEnd = html.indexOf('<')
+        if (textEnd === 0) {
+            let startTagMatch = parseStartTag()
+            if (startTagMatch) {
+                start(startTagMatch.tagName, startTagMatch.attrs)
+                continue // 如果开始标签匹配完毕后 继续下一次匹配
+            }
+
+            let endTagMatch = html.match(endTag)
+            if (endTagMatch) {
+                advance(endTagMatch[0].length)
+                end(endTagMatch[1])
+                continue
+            }
+        }
+
+        let text
+        if (textEnd >= 0) {
+            text = html.substring(0, textEnd)
+        }
+
+        if (text) {
+            advance(text.length)
+            chars(text)
+        }
+    }
+
+    function advance(n) {
+        html = html.substring(n)
+    }
+
+    function parseStartTag() {
+        let start = html.match(startTagOpen)
+        if (start) {
+            const match = {
+                tagName: start[1],
+                attrs: [],
+            }
+            advance(start[0].length)
+
+            let end, attr
+            while (
+                !(end = html.match(startTagClose)) &&
+                (attr = html.match(attribute))
+            ) {
+                // 将属性进行解析
+                advance(attr[0].length) // 将属性去掉
+                match.attrs.push({
+                    name: attr[1],
+                    value: attr[3] || attr[4] || attr[5],
+                })
+            }
+
+            if (end) {
+                // 去掉开始标签的 >
+                advance(end[0].length)
+                return match
+            }
+        }
+    }
+}
+
 export function compileToFunction(template) {
-    console.log(template)
+    let root = parseHTML(template)
     return function render() {}
 }
